9c33ff57b1b17317a943e2ddc39f940552b114e4
Hailey Bates <hailey.bates001@gmail.com>
2022-04-12 10:03:59 -0400

    cleaned up javascript

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 const searchHistory = [];
-const weatherapiUrl = "https://api.openweathermap.org/";
+// const weatherapiUrl = "https://api.openweathermap.org/";
 const apiKey = "b4bd8eb5664abf13763c667594a43480";
 
 
@@ -8,3 +8,16 @@ const apiKey = "b4bd8eb5664abf13763c667594a43480";
 
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
+
+var weatherapiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=b4bd8eb5664abf13763c667594a43480";
+
+function weatherData(weatherApi) {
+    fetch(weatherApi)
+    .then(function(res) {
+       return res.json()
+    })
+    .then(function(data) {
+
+    })
+}
+weatherData(weatherapiUrl);
